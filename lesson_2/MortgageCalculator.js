d6f65f9898195f437c78f5219b2104f54a386f8d
Javi <jmesamarrero@gmail.com>
2021-06-14 22:59:40 +0100

    Mortgage calculator assignmet

---
+++ lesson_2/MortgageCalculator.js
@@ -0,0 +1,177 @@
+/*
+inputs:  * The loan amount
+         * The annual percentage rate (APR)
+         * The loan duration
+outputs: * Monthly interest rate
+         * Loan duration in months
+         * Monthly payment
+
+This is the formula to use --->  m = p * (j / (1 - Math.pow((1 + j), (-n))))
+m = montly payment
+p = loan amount
+j = monthly interest rate ---> is the annual percentage rate / 12
+n = loan duration in month
+
+
+Examples
+
+Loan amount of $5000  at a 6% rate for 5 years =
+$96.66 every month / total paid $5799.84 / total interest $799.84
+
+Loan amount of $10000 at 3.45% rate for 8 years and 3 month =
+$116.21 every month / total paid $11504.82/ total interest $1504.82
+
+Loan amount of $3500 at 0% rate for 2 years and 4 month =
+$125.00 every month / total paid $3500.00 / total interest $0.00
+
+Loan amount of $23540.45 at 5.42% rate for 5 years and 7 month =
+$407.98 every month / total paid $27334.41 / total interest $3793.96
+
+
+Algorithm
+
+-Ask for the loan amount in dollars
+-Ask for the annual percentage rate in % ---> five per cent = 5
+-Ask for the loan duration in years and months ---> yy/mm
+-Calculate Monthly interest rate = annual percentage rate / 12
+-Calculate loan duration in month
+-Calculate montly paiments ---> m = p * (j / (1 - Math.pow((1 + j), (-n))))
+-Calculate total paid ---> montly paiments * loan duration in month
+-Calculate total interest paid ---> total paid - loan amount
+
+*/
+
+const readline = require('readline-sync');
+
+function prompt (string) {
+  console.log(`---> ${string}`);
+}
+
+function invalidInput(num) {
+  return Number.isNaN(num) === true ||
+    num < 0 ||
+    num === '';
+}
+
+function invalidLoanDuration(input) {
+  if (input.includes('/')) {
+    let arr = input.split('/');
+    return ((isNaN(arr[0]) || isNaN(arr[1])) ||
+      (arr[0] < 0 || arr[1] < 0) ||
+      (arr[0] === '' || arr[1] === ''));
+  }
+  return true;
+}
+
+function invalidAnswer(string) {
+  return (string[0] !== 'n' && string[0] !== 'y');
+}
+
+function CalcMonthlyInterestRate(num) {
+  return (num / 100) / 12;
+}
+
+function MonthlyPaiment(loan, monthlyInterest, loanDuration) {
+  return loan * (monthlyInterest /
+    (1 - Math.pow((1 + monthlyInterest), (-loanDuration))));
+}
+
+function AskForLoanAmount() {
+  prompt('Please introduce the loan amount:');
+  let loanAmount = parseFloat(readline.question());
+
+  while (invalidInput(loanAmount)) {
+    prompt('Please introduce a valid loan amount. The loan amount must be a number greater than zero');
+    loanAmount = parseFloat(readline.question());
+  }
+  return loanAmount;
+}
+
+function AskForAnualPercentageRate() {
+  prompt('Please introduce the Anual Percentage Rate: Example for "5.5%" introduce "5.5"');
+  let anualPercentageRate = parseFloat(readline.question());
+
+  while (invalidInput(anualPercentageRate)) {
+    prompt('Please introduce a valid Anual Percentage Rate. Example: For "5.5%" introduce "5.5"');
+    anualPercentageRate = parseFloat(readline.question());
+  }
+  return anualPercentageRate;
+}
+
+function AskForLoanDuration() {
+  prompt('Please introduce the loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
+  let loanDuration = readline.question();
+
+  while (invalidLoanDuration(loanDuration)) {
+    prompt('Please introduce a valid loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
+    loanDuration = readline.question();
+  }
+
+  return loanDuration;
+}
+
+function AskForNewCalculation() {
+  prompt('Would you like to perform another loan calculation? (y/n)');
+  let answer = readline.question().toLowerCase();
+
+  while (invalidAnswer(answer)) {
+    prompt('Please answer "y" for yes or "n" for no');
+    answer = readline.question().toLowerCase();
+  }
+  return answer;
+}
+
+while (true) {
+
+  console.clear();
+  console.log('-----------------------------------------------------');
+  console.log('|      Welcome to the Mortgage Calculator           |');
+  console.log('-----------------------------------------------------');
+  console.log('\n');
+
+  let loanAmount = AskForLoanAmount();
+  let anualPercentageRate = AskForAnualPercentageRate();
+  let loanDuration = AskForLoanDuration();
+
+
+  if (anualPercentageRate === 0) {
+    let loanDurationArray = loanDuration.split('/');
+
+    let loanDurationMonths = (Number(loanDurationArray[0]) * 12) +
+      Number(loanDurationArray[1]);
+
+    let monthlyPaiment = loanAmount / loanDurationMonths;
+    prompt(`You will have to pay $${monthlyPaiment.toFixed(2)} every month.`);
+    prompt(`The total amount paid will be $${loanAmount.toFixed(2)}.`);
+    prompt(`The total interest amount will be $0.`);
+
+  } else {
+    let loanDurationArray = loanDuration.split('/');
+
+    let loanDurationMonths = (Number(loanDurationArray[0]) * 12) +
+      Number(loanDurationArray[1]);
+
+    let monthlyPercentageRate = CalcMonthlyInterestRate(anualPercentageRate);
+
+    let monthlyPaiment = MonthlyPaiment (
+      loanAmount, monthlyPercentageRate, loanDurationMonths);
+
+    let totalPaid = monthlyPaiment * loanDurationMonths;
+
+    let totalInterest = totalPaid - loanAmount;
+
+    prompt(`You will have to pay $${monthlyPaiment.toFixed(2)} every month.`);
+    prompt(`The total amount paid will be $${totalPaid.toFixed(2)}.`);
+    prompt(`The total interest amount will be $${totalInterest.toFixed(2)}.\n`);
+  }
+
+  let answer = AskForNewCalculation();
+
+  if (answer === 'n') {
+    console.log('-----------------------------------------------------');
+    console.log('|               Aplication closed                   |');
+    console.log('-----------------------------------------------------\n\n');
+    break;
+  }
+
+}
--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -16,45 +16,64 @@ function invalidNumber(num) {
 
 promp('Welcome to Calculator!');
 
-promp("What's is the first number?");
-let number1 = readline.question();
+let repeat = true;
 
-while (invalidNumber(number1)) {
-  promp("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+while (repeat) {
+
+  promp("What's is the first number?");
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    promp("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
+
+  promp("What's is the second number?");
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    promp("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+
+  promp('What operation would you like to perform?\n1) Add 2) Substract 3) Multiply 4) Divide');
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    promp('Must choose 1, 2, 3 or 4');
+    operation = readline.question();
+  }
+
+  let output;
+
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  promp(`The result is ${output}`);
+
+  promp('Do you want to make another operation? "y" yes or "n" no');
+  let answer = readline.question();
+
+  while (!['y', 'n'].includes(answer)) {
+    answer = readline.question(promp("Didn't understand you, push \"y\" for yes or \"n\" for no"));
+  }
+
+  if (answer === 'n') {
+    repeat = false;
+  }
 }
 
-promp("What's is the second number?");
-let number2 = readline.question();
+promp('Bye bye!');
 
-while (invalidNumber(number2)) {
-  promp("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-promp('What operation would you like to perform?\n1) Add 2) Substract 3) Multiply 4) Divide');
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  promp('Must choose 1, 2, 3 or 4');
-  operation = readline.question();
-}
-
-let output;
-
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-promp(`The result is ${output}`);
