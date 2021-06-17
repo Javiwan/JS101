3f6c7207b68e4425fcf9ea8c926cbb002ddf25d5
Javi <jmesamarrero@gmail.com>
2021-06-17 19:05:58 +0100

    Improvements made to MortgageCalculator.js

--- lesson_2/MortgageCalculator.js
+++ lesson_2/MortgageCalculator.js
@@ -43,117 +43,115 @@ Algorithm
 
 const readline = require('readline-sync');
 
-function Prompt (string) {
+function prompt (string) {
   console.log(`---> ${string}`);
 }
 
-function InvalidInput(num) {
+function invalidInput(num) {
   return Number.isNaN(num) === true ||
     num < 0 ||
     num === '';
 }
 
-function InvalidLoanDuration(input) {
+function invalidLoanDuration(input) {
   if (input.includes('/')) {
     let arr = input.split('/');
+    console.log(arr[0], arr[1]);
     return ((isNaN(arr[0]) || isNaN(arr[1])) ||
-      (arr[0] < 0 || arr[1] < 0) ||
+      (((Number(arr[0]) < 0 || Number(arr[1]) < 0) ) ||
+      ((Number(arr[0]) === 0) && (Number(arr[1]) === 0))) ||
       (arr[0] === '' || arr[1] === ''));
   }
   return true;
 }
 
-function InvalidAnswer(string) {
-  return (string[0] !== 'n' && string[0] !== 'y');
-}
-
-function CalcMonthlyInterestRate(num) {
+function calcMonthlyInterestRate(num) {
   return (num / 100) / 12;
 }
 
-function MonthlyPaiment(loan, monthlyInterest, loanDuration) {
+function MonthlyPaiments(loan, monthlyInterest, loanDuration) {
   return loan * (monthlyInterest /
     (1 - Math.pow((1 + monthlyInterest), (-loanDuration))));
 }
 
-function LoanDurationInMonths(loanDuration) {
+function loanDurationInMonths(loanDuration) {
   let loanDurationArray = loanDuration.split('/');
 
   return (parseInt(loanDurationArray[0], 10) * 12) +
     parseInt(loanDurationArray[1], 10);
 }
 
-function AskForLoanAmount() {
-  Prompt('Please introduce the loan amount:');
+function askForLoanAmount() {
+  prompt('Please introduce the loan amount:');
   let loanAmount = parseFloat(readline.question());
 
-  while (InvalidInput(loanAmount)) {
-    Prompt('Please introduce a valid loan amount. The loan amount must be a number greater than zero');
+  while (invalidInput(loanAmount)) {
+    prompt('Please introduce a valid loan amount. The loan amount must be a number greater than zero');
     loanAmount = parseFloat(readline.question());
   }
   return loanAmount;
 }
 
-function AskForAnualPercentageRate() {
-  Prompt('Please introduce the Anual Percentage Rate: Example for "5.5%" introduce "5.5"');
+function askForAnualPercentageRate() {
+  prompt('Please introduce the Anual Percentage Rate: Example for "5.5%" introduce "5.5"');
   let anualPercentageRate = parseFloat(readline.question());
 
-  while (InvalidInput(anualPercentageRate)) {
-    Prompt('Please introduce a valid Anual Percentage Rate. Example: For "5.5%" introduce "5.5"');
+  while (invalidInput(anualPercentageRate)) {
+    prompt('Please introduce a valid Anual Percentage Rate. Example: For "5.5%" introduce "5.5"');
     anualPercentageRate = parseFloat(readline.question());
   }
   return anualPercentageRate;
 }
 
-function AskForLoanDuration() {
-  Prompt('Please introduce the loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
+function askForLoanDuration() {
+  prompt('Please introduce the loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
   let loanDuration = readline.question();
 
-  while (InvalidLoanDuration(loanDuration)) {
-    Prompt('Please introduce a valid loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
+  while (invalidLoanDuration(loanDuration)) {
+    prompt('Please introduce a valid loan duration in years/months. Example for 2 years and 11 months introduce "2/11"');
     loanDuration = readline.question();
   }
 
   return loanDuration;
 }
 
-function NoPercentageRateLoanCalc(loanDuration, loanAmount) {
+function noPercentageRateLoanCalc(loanDuration, loanAmount) {
   let totalInterest = 0;
 
-  let loanDurationMonths = LoanDurationInMonths(loanDuration);
+  let loanDurationMonths = loanDurationInMonths(loanDuration);
 
   let monthlyPaiment = loanAmount / loanDurationMonths;
 
-  PrintResults(monthlyPaiment, loanAmount, totalInterest);
+  printResults(monthlyPaiment, loanAmount, totalInterest);
 }
 
-function LoanCalc(loanDuration, anualPercentageRate, loanAmount) {
-  let loanDurationMonths = LoanDurationInMonths(loanDuration);
+function loanCalc(loanDuration, anualPercentageRate, loanAmount) {
+  let loanDurationMonths = loanDurationInMonths(loanDuration);
 
-  let monthlyPercentageRate = CalcMonthlyInterestRate(anualPercentageRate);
+  let monthlyPercentageRate = calcMonthlyInterestRate(anualPercentageRate);
 
-  let monthlyPaiment = MonthlyPaiment (
+  let monthlyPaiment = MonthlyPaiments (
     loanAmount, monthlyPercentageRate, loanDurationMonths);
 
   let totalPaid = monthlyPaiment * loanDurationMonths;
 
   let totalInterest = totalPaid - loanAmount;
 
-  PrintResults(monthlyPaiment, totalPaid, totalInterest);
+  printResults(monthlyPaiment, totalPaid, totalInterest);
 }
 
-function PrintResults(monthlyPaiment, loanAmount, totalInterest) {
-  Prompt(`You will have to pay $${monthlyPaiment.toFixed(2)} every month.`);
-  Prompt(`The total amount paid will be $${loanAmount.toFixed(2)}.`);
-  Prompt(`The total interest amount will be $${totalInterest.toFixed(2)}.`);
+function printResults(monthlyPaiment, loanAmount, totalInterest) {
+  prompt(`You will have to pay $${monthlyPaiment.toFixed(2)} every month.`);
+  prompt(`The total amount paid will be $${loanAmount.toFixed(2)}.`);
+  prompt(`The total interest amount will be $${totalInterest.toFixed(2)}.`);
 }
 
-function AskForNewCalculation() {
-  Prompt('Would you like to perform another loan calculation? (y/n)');
+function askForNewCalculation() {
+  prompt('Would you like to perform another loan calculation? (y/n)');
   let answer = readline.question().toLowerCase();
 
-  while (InvalidAnswer(answer)) {
-    Prompt('Please answer "y" for yes or "n" for no');
+  while (!['y', 'n'].includes(answer)) {
+    prompt('Please answer "y" for yes or "n" for no');
     answer = readline.question().toLowerCase();
   }
   return answer;
@@ -167,19 +165,19 @@ while (true) {
   console.log('-----------------------------------------------------');
   console.log('\n');
 
-  let loanAmount = AskForLoanAmount();
-  let anualPercentageRate = AskForAnualPercentageRate();
-  let loanDuration = AskForLoanDuration();
+  let loanAmount = askForLoanAmount();
+  let anualPercentageRate = askForAnualPercentageRate();
+  let loanDuration = askForLoanDuration();
 
 
   if (anualPercentageRate === 0) {
-    NoPercentageRateLoanCalc(loanDuration, loanAmount);
+    noPercentageRateLoanCalc(loanDuration, loanAmount);
 
   } else {
-    LoanCalc(loanDuration, anualPercentageRate, loanAmount);
+    loanCalc(loanDuration, anualPercentageRate, loanAmount);
   }
 
-  let answer = AskForNewCalculation();
+  let answer = askForNewCalculation();
 
   if (answer === 'n') {
     console.log('-----------------------------------------------------');
